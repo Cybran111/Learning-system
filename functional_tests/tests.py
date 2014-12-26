c021146c1015e3d6e1569f4454aa9ebae1d30610
Nikita "Cybran" Lyubchich <cybran111@gmail.com>
2014-12-26 11:42:27 +0200

    Functional test hotfix

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -16,8 +16,10 @@ class NewVisitorTest(LiveServerTestCase):
         for course in course_data:
             Course.objects.create(title=course["title"], description=course["description"])
 
+
+
         self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(2)
+        self.browser.implicitly_wait(5)
 
     def tearDown(self):
         self.browser.quit()
@@ -25,7 +27,6 @@ class NewVisitorTest(LiveServerTestCase):
     def test_can_look_at_course_list(self):
         # Alice has heard about a cool new course platform.
         # She goes to check out its homepage
-
 
         self.browser.get(self.live_server_url)
 
@@ -40,6 +41,7 @@ class NewVisitorTest(LiveServerTestCase):
 
     def test_can_go_to_course_page(self):
 
+        self.browser.get(self.live_server_url)
         _list = self.browser.find_element_by_class_name('list-group')
         first_course = _list.find_element_by_class_name('list-group-item')
         first_course.click()
