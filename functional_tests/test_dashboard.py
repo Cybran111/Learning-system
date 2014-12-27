0382d5df97d0c66103ef184763bf10c3e882480c
Nikita "Cybran" Lyubchich <cybran111@gmail.com>
2014-12-27 13:55:10 +0200

    Dashboard test are disabled for better times

--- functional_tests/test_dashboard.py
+++ functional_tests/test_dashboard.py
@@ -15,21 +15,21 @@ class NewVisitorTest(LiveServerTestCase):
 
     def test_can_sign_up(self):
         ## John wanna sign up to E-Learning System
-
-        self.browser.get(self.live_server_url)
-
-        # He finds the button "Sign up" and click on it
-        self.browser.find_element_by_id('signup').click()
-
-        # He writes his email..
-        email_inputbox = self.browser.find_element_by_id('email')
-        email_inputbox.send_keys("john@example.com")
-
-        # and his password..
-        password_inputbox = self.browser.find_element_by_id('password')
-        password_inputbox.send_keys("verysecretpassword")
-
-        # and he presses the Register button
-        self.browser.find_element_by_id('register').click()
-
         self.fail("Finish the test")
+
+        # self.browser.get(self.live_server_url)
+        #
+        # # He finds the button "Sign up" and click on it
+        # self.browser.find_element_by_id('signup').click()
+        #
+        # # He writes his email..
+        # email_inputbox = self.browser.find_element_by_id('email')
+        # email_inputbox.send_keys("john@example.com")
+        #
+        # # and his password..
+        # password_inputbox = self.browser.find_element_by_id('password')
+        # password_inputbox.send_keys("verysecretpassword")
+        #
+        # # and he presses the Register button
+        # self.browser.find_element_by_id('register').click()
+
