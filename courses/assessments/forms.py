fa57b5c42a57fb11c350969a8bc0d132f2975872
Nikita Lyubchich <cybran111@gmail.com>
2015-05-07 14:44:23 +0300

    Possible to watch Assessment page

--- courses/assessments/forms.py
+++ courses/assessments/forms.py
@@ -5,9 +5,27 @@ from django import forms
 # )
 
 
+# class AnswerCheckBoxSelectMultiple():
+#     """
+#     For fixing bad labels
+#     """
+#     def __init__(self, question_id, *args, **kwargs):
+#         self.question_id = question_id
+#         super(AnswerCheckBoxSelectMultiple, self).__init__(*args, **kwargs)
+#
+#     def id_for_label(self, id_):
+#         if id_:
+#             id_ += str(self.question_id+100500)
+#         return id_
+
+
 class QuestionForm(forms.Form):
-    def __init__(self, *args, **kwargs):
-        answers = kwargs.pop('answers')
-        answers = tuple((answer.number, answer.text) for answer in answers)
+    def __init__(self, text, number, answers, *args, **kwargs):
+        self.text = text
+        self.number = number
+        answers = tuple(("%d-%d" % (number, answer.number), answer.text) for answer in answers)
         super(QuestionForm, self).__init__(*args, **kwargs)
-        self.fields["answers"] = forms.MultipleChoiceField(choices=answers, widget=forms.CheckboxSelectMultiple())
+        self.fields["answers"] = forms.MultipleChoiceField(choices=answers,
+                                                           widget=forms.CheckboxSelectMultiple(
+                                                               attrs={"id": unicode(number)}
+                                                           ))
--- courses/assessments/views.py
+++ courses/assessments/views.py
@@ -25,16 +25,18 @@ def assessment_overview(request, course_id, week_id, assessment_id):
 @login_required
 def assessment_attempt(request, course_id, week_id, assessment_id):
     questionset = QuestionSet.objects.get(course=course_id, week=week_id, number=assessment_id)
-    forms = tuple(QuestionForm(answers=tuple(answer for answer in question.possibleanswer_set.all()))
-                  for question in questionset.question_set.all())
+    # forms = tuple(QuestionForm(answers=tuple(answer for answer in question.possibleanswer_set.all()))
+    #               for question in questionset.question_set.all())
 
-    questionset = (questionset.title, questionset.description, tuple(
-        (
-            question.text,
-            QuestionForm(answers=tuple(answer for answer in question.possibleanswer_set.all()))
+    questionset = {
+        "title": questionset.title,
+        "description": questionset.description,
+        "questions": (
+            QuestionForm(text=question.text, number=question.number, answers=(answer for answer in question.possibleanswer_set.all()))
+            for question in questionset.question_set.all()
         )
-        for question in questionset.question_set.all()
-    ))
+
+    }
     # print questionset
     # return render(request, "courses/assessments/assessment_attempt.html", {"questionset": questionset, "forms": forms})
     return render(request, "courses/assessments/assessment_attempt.html", {"questionset": questionset})
