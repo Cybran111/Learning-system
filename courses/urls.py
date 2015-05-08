d284699fb17db446124d3d0b77c673d457de5f65
Nikita Lyubchich <cybran111@gmail.com>
2015-05-08 19:22:33 +0300

    Fixed accidentally removed route

--- courses/urls.py
+++ courses/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url, include
 
 __author__ = 'cybran'
 
-urlpatterns = patterns('courses.views', url(r'^new$', "create_course_view", name='new_course'),
+urlpatterns = patterns('courses.views',
                        url(r'^(?P<course_id>\d+)/$', "course_dashboard_view", name='course_dashboard'),
                        url(r'^(?P<course_id>\d+)/overview/$', "course_overview_view", name='course_overview'),
                        url(r'^(?P<course_id>\d+)/lectures/$', "lecture_list_view", name='lectures'),
@@ -10,11 +10,8 @@ urlpatterns = patterns('courses.views', url(r'^new$', "create_course_view", name
                            "lecture_view", name="lecture"),
                        url(r'^(?P<course_id>\d+)/news/$', "news_view", name='news'),
 
-
-
-                       url(r'^(?P<course_id>\d+)/assignments/', include('courses.assignments.urls',
-                                                                        namespace='assignments')),
-
-                       url(r'^(?P<course_id>\d+)/assessments/', include('courses.assessments.urls',
-                                                                        namespace='assessments')),
-                       )
+                       url(r'^(?P<course_id>\d+)/assignments/',
+                           include('courses.assignments.urls', namespace='assignments')),
+                       url(r'^(?P<course_id>\d+)/assessments/',
+                           include('courses.assessments.urls', namespace='assessments')),
+)
