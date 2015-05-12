85cc97ec7ae5d9f561b5efc79c532bfde6471d41
Nikita Lyubchich <cybran111@gmail.com>
2015-05-12 16:44:59 +0300

    Fixing spacing

--- courses/templatetags/course_tags.py
+++ courses/templatetags/course_tags.py
@@ -48,3 +48,22 @@ def do_xextends(parser, token):
     return XExtendsNode(do_extends(parser, token), kwargs)
 
 register.tag('xextends', do_xextends)
+
+
+from django.template import Library
+from django.template.defaultfilters import stringfilter
+from django.utils.html import conditional_escape
+from django.utils.safestring import mark_safe
+import re
+
+# register = Library()
+
+@stringfilter
+def spacify(value, autoescape=None):
+    if autoescape:
+        esc = conditional_escape
+    else:
+        esc = lambda x: x
+    return mark_safe(re.sub('\s', '&'+'nbsp;', esc(value)))
+spacify.needs_autoescape = True
+register.filter(spacify)
